6289c965744e41954e62ac2236022a9916e16110
Michael Wnuk <mwnuk@theonion.com>
2014-06-02 14:55:10 -0500

    more inline

--- build/onion-editor.js
+++ build/onion-editor.js
@@ -9672,6 +9672,13 @@ define('scribe-plugin-inline-objects',[],function () {
         function configLoaded(data) {
           templates = data;
           $(".embed-tools button", editorEl).click(insertObject);
+
+          $(".inline-tools button", editorEl.parentNode).click(function(event) {
+            var name = $(event.target).attr("name");
+            if (typeof actions[name] === "function") {
+              actions[name]();
+            }
+          });
         }
         
         function insertObject(event) {
@@ -9693,11 +9700,6 @@ define('scribe-plugin-inline-objects',[],function () {
           ]);
           $(".embed-tools", editorEl).removeClass("active");
           activeBlock = undefined;
-        }
-
-        // Edit action
-        function edit(event) {
-
         }
 
 
@@ -9743,44 +9745,158 @@ define('scribe-plugin-inline-objects',[],function () {
           else {
             hideToolbar();
           }
-          function hideToolbar() {
-            $(".inline-tools").hide();
-            $(editorEl).removeClass("inline-active")
-          }
 
-          function showToolbar() {
-            var el = $(activeElement);
-            var pos = el.position();
-            $(editorEl).addClass("inline-active");
-            
-            //set size buttons.
-
-            $(".inline-tools .size", editorEl)
-              .html($(activeElement).attr("data-size"));
-
-            //set crop
-            $(".inline-tools .crop", editorEl)
-              .html($(activeElement).attr("data-crop"));
-
-            $(".inline-tools", editorEl)
-              .css({
-                top: pos.top + parseInt(el.css('margin-top')), 
-                left: pos.left + parseInt(el.css('margin-left')) + parseInt($(".editor", editorEl).css('margin-left')),
-                width: el.width(),
-                height: el.height()
-              })
-              .show();
-          }
         });
 
-        // Caption (keep this in for now, for simplicity)
+        function hideToolbar() {
+          $(".inline-tools").hide();
+          $(editorEl).removeClass("inline-active")
+        }
+
+        function showToolbar() {
+          var el = $(activeElement);
+          var pos = el.position();
+          $(editorEl).addClass("inline-active");
+          
+          //set size buttons.
+
+          $(".inline-tools .size", editorEl)
+            .html($(activeElement).attr("data-size"));
+
+          //set crop
+          $(".inline-tools .crop", editorEl)
+            .html($(activeElement).attr("data-crop"));
+
+          $(".inline-tools", editorEl)
+            .css({
+              top: pos.top + parseInt(el.css('margin-top')), 
+              left: pos.left + parseInt(el.css('margin-left')) + parseInt($(".editor", editorEl).css('margin-left')),
+              width: el.width(),
+              height: el.height()
+            })
+            .show();
+        }
+
+
+        function getSizes() {
+          return  templates[$(activeElement).attr("data-type")].size;
+        }
+
+        function getCrops() {
+          return templates[$(activeElement).attr("data-type")].crop;
+        }
+
+        //TODO: Determine how to handle two adjacent inline elements. Probably skip over?
+        var actions = {
+
+          inline_caption: function() {
+            var caption = prompt("Caption", 
+              $(".caption", activeElement).html()
+            );
+            if (caption) {
+              scribe.transactionManager.run(function () {
+                $(".caption", activeElement).html(caption);
+              });
+            }
+          },
+          //TODO: size/crop isn't working right after you hit the "HUGE" size in images
+          inline_size: function() {
+            var l = getSizes();
+            toggleAttribute("size", l);
+
+            var currentCrop = $(activeElement).attr("data-crop");
+            var cropOptions = getCrops();
+            //this crop isn't available for the new size option
+            if (cropOptions.indexOf(currentCrop) === -1) {
+              scribe.transactionManager.run(function () {
+                setValue("crop", cropOptions[0]);
+              });
+            }
+          },
+          inline_crop: function() {
+            var l = templates[$(activeElement).attr("data-type")].crop;
+            toggleAttribute("crop", l);
+          },
+          inline_up: function() {
+            var previousBlock = $(activeElement).prev()[0];
+            if (previousBlock) {
+              var top = $(activeElement).offset().top;
+              scribe.transactionManager.run(function () {
+                $(activeElement).after(previousBlock);
+              });
+              showToolbar();
+              var newTop = $(activeElement).offset().top;
+              window.scrollBy(0, newTop - top)
+            }
+          },
+          inline_down: function() {
+            var nextBlock = $(activeElement).next()[0];
+            if (nextBlock) {
+              var top = $(activeElement).offset().top;
+              scribe.transactionManager.run(function () {
+                $(activeElement).before(nextBlock)
+              });
+              showToolbar()
+              var newTop = $(activeElement).offset().top;
+              window.scrollBy(0, newTop - top)
+            }
+          },
+          inline_remove: function () {
+            scribe.transactionManager.run(function () {
+              $(activeElement).remove();
+            });
+            hideToolbar();
+          },  
+          inline_edit: function () {
+            /* I think this should be a bit more like insert.
+            We establish an onChange callback where we update the template with new values. 
+            For now, the module is responsible for making modifications to the markup. 
+
+            */
+            scribe.on("inline:" + $(activeElement).attr("data-type"), 
+              {
+                element: activeElement,
+                onChange: function(element, values) {
+                  var type = $(element).attr("data-type");
+                  element.outerHTML = 
+                    render(
+                      templates[type].template,
+                      $.extend(templates[type].defaults, values) 
+                    )
+                }
+              }
+            )
+          }
+        }
+
+        function toggleAttribute(attribute, list) {
+          var currentValue = $(activeElement).attr("data-" + attribute);
+          var index = list.indexOf(currentValue) + 1;
+          if (index >= list.length)
+            index = 0;
+
+          setValue(attribute, list[index]);
+          if (typeof window.picturefill === "function") {
+            setTimeout(window.picturefill, 100);
+          }
+           
+        } 
+
+        function setValue(attribute, value) {
+          var currentValue = $(activeElement).attr("data-" + attribute);
+          $(activeElement)
+            .removeClass(attribute + "-" + currentValue)
+            .addClass(attribute + "-" + value)
+            .attr("data-" + attribute, value)
+          showToolbar();
+        }
 
 
         function render(html, dict) {
           for (var k in dict) {
-              if (k) {
-                  html = html.replace(new RegExp("{{" + k + "}}", 'g'), dict[k]);
-              }
+            if (k) {
+              html = html.replace(new RegExp("{{" + k + "}}", 'g'), dict[k]);
+            }
           }
           return html;
         }
