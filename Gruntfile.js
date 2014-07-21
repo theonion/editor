b5c25a2684a49b9dd3bc1083a6bf768fec4df1cd
Michael Wnuk <mwnuk@theonion.com>
2014-07-21 11:30:09 -0500

    css build process w/ less

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,13 +12,26 @@ module.exports = function(grunt) {
     },
     watch: {
       scripts: {
-        files: ['src/*.js', 'src/*/*.js'],
-        tasks: ['requirejs'],
+        files: ['src/js/*.js', 'src/js/*/*.js', 'src/less/*/*.js','src/less/*/*.js'],
+        tasks: ['requirejs', 'less'],
+      }
+    },
+    less: {
+      production: {
+        options: {
+          paths: ["src/less"],
+          cleancss: false
+        },
+        files: {
+          'build/editor-main.css': ['src/less/editor/*'],
+          'build/inline.css': ['src/less/inline.less']
+        }
       }
     }
   });
   // Load external tasks
+  grunt.loadNpmTasks('grunt-contrib-less');
   grunt.loadNpmTasks('grunt-contrib-requirejs');
   grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.registerTask('default', 'requirejs');
+  grunt.registerTask('default', 'requirejs', 'less');
 };
