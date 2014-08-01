e3bdb6de942ff7e676fcbf6482edcd51511c33b2
Chris Sinchok <csinchok@theonion.com>
2014-08-01 13:27:35 -0500

    Adding onion editor to the coverage, and also doin' a lil resolving of the ol bower

--- karma.conf.js
+++ karma.conf.js
@@ -67,6 +67,7 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+      'src/js/onion-editor.js': 'coverage',
       'src/js/*/*.js': 'coverage'
     },
 
