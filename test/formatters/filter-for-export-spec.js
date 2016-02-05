53242a4687b9537645f1bd1616d824c82fdb1b4e
collin <collintmiller@gmail.com>
2016-02-05 14:27:40 -0600

    Update filter-for-export-spec.js

--- test/formatters/filter-for-export-spec.js
+++ test/formatters/filter-for-export-spec.js
@@ -6,17 +6,22 @@ define(['scribe', 'formatters/filter-for-export'], function (Scribe, FilterForEx
       var scribe = new Scribe(element, { allowBlockElements: true });
       scribe.use(new FilterForExport());
       scribe.setContent('<p>I am not filtered!</p>');
-
+      
+      // First, check that default filtering does not alter the content.
       expect(scribe.getContent()).toEqual('<p>I am not filtered!</p>');
 
+      // Abuse the poor <p> tag prototype for testing purpsoses.
       HTMLParagraphElement.prototype.filterForExport = function () {
         this.innerText = "I was filtered!";
       }
 
+      // Note that the content is filtered.
       expect(scribe.getContent()).toEqual('<p>I was filtered!</p>');
 
+      // Restore order to the universe.
       delete HTMLParagraphElement.prototype.filterForExport;
 
+      // And confirm the content in the editor was not damaged.
       expect(scribe.getContent()).toEqual('<p>I am not filtered!</p>');
     });
   });
