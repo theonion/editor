d24672b8348a173e2788e6e5536e1e963800d54c
Collin Miller <collintmiller@gmail.com>
2016-02-05 14:39:45 -0600

    firefox prototypes don't update already created elements

--- test/formatters/filter-for-export-spec.js
+++ test/formatters/filter-for-export-spec.js
@@ -5,18 +5,20 @@ define(['scribe', 'formatters/filter-for-export'], function (Scribe, FilterForEx
       var element = document.createElement('div');
       var scribe = new Scribe(element, { allowBlockElements: true });
       scribe.use(new FilterForExport());
-      scribe.setContent('<p>I am not filtered!</p>');
 
+      scribe.setContent('<p>I am not filtered!</p>');
       expect(scribe.getContent()).toEqual('<p>I am not filtered!</p>');
 
       HTMLParagraphElement.prototype.filterForExport = function () {
         this.innerText = "I was filtered!";
       }
 
+      scribe.setContent('<p>I am not filtered!</p>');
       expect(scribe.getContent()).toEqual('<p>I was filtered!</p>');
 
       delete HTMLParagraphElement.prototype.filterForExport;
 
+      scribe.setContent('<p>I am not filtered!</p>');
       expect(scribe.getContent()).toEqual('<p>I am not filtered!</p>');
     });
   });
